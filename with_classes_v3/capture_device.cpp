61b26040f5151bbc250f10a009c4e1c86263533a
Jonathan Gilat <63646867+jgilat@users.noreply.github.com>
2021-02-01 08:26:50 +0200

    Add files via upload

---
+++ with_classes_v3/capture_device.cpp
@@ -0,0 +1,111 @@
+#include "capture_device.h"
+
+/* 4 bytes IP address */
+typedef struct ip_address {
+	u_char byte1;
+	u_char byte2;
+	u_char byte3;
+	u_char byte4;
+}ip_address;
+
+/* IPv4 header */
+typedef struct ip_header {
+	u_char	ver_ihl;		// Version (4 bits) + Internet header length (4 bits)
+	u_char	tos;			// Type of service 
+	u_short tlen;			// Total length 
+	u_short identification; // Identification
+	u_short flags_fo;		// Flags (3 bits) + Fragment offset (13 bits)
+	u_char	ttl;			// Time to live
+	u_char	proto;			// Protocol
+	u_short crc;			// Header checksum
+	ip_address	saddr;		// Source address
+	ip_address	daddr;		// Destination address
+	u_int	op_pad;			// Option + Padding
+}ip_header;
+
+/* UDP header*/
+typedef struct udp_header {
+	u_short sport;			// Source port
+	u_short dport;			// Destination port
+	u_short len;			// Datagram length
+	u_short crc;			// Checksum
+}udp_header;
+
+capture_device::capture_device(pcap_if_t* d, char* errbuf) {
+	/* Open the adapter */
+	if ((adhandle = pcap_open(d->name,	// name of the device
+		65536,		// portion of the packet to capture. 
+				   // 65536 grants that the whole packet will be captured on all the MACs.
+		PCAP_OPENFLAG_PROMISCUOUS,			// promiscuous mode
+		1000,		// read timeout
+		NULL,		// remote authentication
+		errbuf		// error buffer
+	)) == NULL)
+	{
+		fprintf(stderr, "\nUnable to open the adapter. %s is not supported by Npcap\n", d->name);
+		return;
+	}
+}
+
+capture_device::~capture_device() {
+	pcap_close(adhandle);
+}
+
+void capture_device::check_datalink() {
+	/* Check the link layer. We support only Ethernet for simplicity. */
+	if (pcap_datalink(adhandle) != DLT_EN10MB)
+	{
+		fprintf(stderr, "\nThis program works only on Ethernet networks.\n");
+		return;
+	}
+}
+
+/* Callback function invoked by libpcap for every incoming packet */
+void capture_device::packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_char* pkt_data)
+{
+	struct tm ltime;
+	char timestr[16];
+	ip_header* ih;
+	udp_header* uh;
+	u_int ip_len;
+	u_short sport, dport;
+	time_t local_tv_sec;
+
+	/*
+	 * Unused variable
+	 */
+	(VOID)(param);
+
+	/* convert the timestamp to readable format */
+	local_tv_sec = header->ts.tv_sec;
+	localtime_s(&ltime, &local_tv_sec);
+	strftime(timestr, sizeof timestr, "%H:%M:%S", &ltime);
+
+	/* print timestamp and length of the packet */
+	printf("%s.%.6d len:%d ", timestr, header->ts.tv_usec, header->len);
+
+	/* retireve the position of the ip header */
+	ih = (ip_header*)(pkt_data +
+		14); //length of ethernet header
+
+	/* retireve the position of the udp header */
+	ip_len = (ih->ver_ihl & 0xf) * 4;
+	uh = (udp_header*)((u_char*)ih + ip_len);
+
+	/* convert from network byte order to host byte order */
+	sport = ntohs(uh->sport);
+	dport = ntohs(uh->dport);
+
+	/* print ip addresses and udp ports */
+	printf("%d.%d.%d.%d.%d -> %d.%d.%d.%d.%d\n",
+		ih->saddr.byte1,
+		ih->saddr.byte2,
+		ih->saddr.byte3,
+		ih->saddr.byte4,
+		sport,
+		ih->daddr.byte1,
+		ih->daddr.byte2,
+		ih->daddr.byte3,
+		ih->daddr.byte4,
+		dport);
+}
